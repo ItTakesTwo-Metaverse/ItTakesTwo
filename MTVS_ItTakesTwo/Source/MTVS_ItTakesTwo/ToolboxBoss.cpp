6f535616db6d7670c16d586fdb25589b91729f2d
lee <hyeonmin1012@gmail.com>
2024-07-27 12:30:20 +0900

    [feat] 보스FSM 설계, 콜리전 프리셋 추가

--- MTVS_ItTakesTwo/Source/MTVS_ItTakesTwo/ToolboxBoss.cpp
+++ MTVS_ItTakesTwo/Source/MTVS_ItTakesTwo/ToolboxBoss.cpp
@@ -3,7 +3,8 @@
 
 #include "ToolboxBoss.h"
 #include "Components/SkeletalMeshComponent.h"
-#include "Components/BoxComponent.h"
+#include "ToolBoxBossFSM.h"
+#include "Components/SceneComponent.h"
 
 // Sets default values
 AToolboxBoss::AToolboxBoss()
@@ -12,11 +13,12 @@ AToolboxBoss::AToolboxBoss()
 	PrimaryActorTick.bCanEverTick = true;
 	
 	// 보스 몸체
-
-	//BossMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BossMesh"));
+	
 	GetMesh()->SetupAttachment(RootComponent);
 	GetMesh()->SetRelativeLocation(FVector(0, 0, 450));
 	GetMesh()->SetRelativeScale3D(FVector(5, 10, 10));
+	GetMesh()->SetCollisionProfileName(TEXT("Boss"));
+
 
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> BossMeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Engine/EngineMeshes/SkeletalCube.SkeletalCube'"));
 	if (BossMeshAsset.Succeeded())
@@ -30,6 +32,7 @@ AToolboxBoss::AToolboxBoss()
 	LeftArmMesh->SetRelativeLocation(FVector(0,-12,20));
 	LeftArmMesh->SetRelativeRotation(FRotator(-10, 90, -90));
 	LeftArmMesh->SetRelativeScale3D(FVector(0.05,0.3,0.05));
+	LeftArmMesh->SetCollisionProfileName(TEXT("Boss"));
 
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> LeftArmMeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Engine/EditorMeshes/SkeletalMesh/DefaultSkeletalMesh.DefaultSkeletalMesh'"));
 	if (LeftArmMeshAsset.Succeeded())
@@ -43,6 +46,7 @@ AToolboxBoss::AToolboxBoss()
 	RightArmMesh->SetRelativeLocation(FVector(0, 12, 20));
 	RightArmMesh->SetRelativeRotation(FRotator(-10, 270, -90));
 	RightArmMesh->SetRelativeScale3D(FVector(0.05, 0.3, 0.05));
+	RightArmMesh->SetCollisionProfileName(TEXT("Boss"));
 
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> RightArmMeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Engine/EditorMeshes/SkeletalMesh/DefaultSkeletalMesh.DefaultSkeletalMesh'"));
 	if (RightArmMeshAsset.Succeeded())
@@ -51,13 +55,19 @@ AToolboxBoss::AToolboxBoss()
 	}
 
 	// 보스 오른손
-	RightHandBox = CreateDefaultSubobject<UBoxComponent>(TEXT("RightHandBox"));
-	RightHandBox->SetupAttachment(RightArmMesh, TEXT("RightHandSocket"));
 	RightHandMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightHandMesh"));
-	RightHandMesh->SetupAttachment(RightHandBox);
-	RightHandBox->SetRelativeLocation(FVector(0, 0, -240));
-	RightHandBox->SetRelativeRotation(FRotator(0, 0, -90));
+	RightHandMesh->SetupAttachment(RightArmMesh, TEXT("RightHandSocket"));
+	RightHandMesh->SetRelativeLocation(FVector(0, 0, -260));
+	RightHandMesh->SetRelativeRotation(FRotator(0, 0, -90));
+	RightHandMesh->SetGenerateOverlapEvents(true);
+	RightHandMesh->SetCollisionProfileName(TEXT("Boss"));
 
+	// 충돌체
+	RightHandMesh->OnComponentBeginOverlap.AddDynamic(this, &AToolboxBoss::OnMyBossBeginOverlap);
+
+	// FSM 컴포넌트 추가
+	fsm = CreateDefaultSubobject<UToolBoxBossFSM>(TEXT("FSM"));
+	
 
 }
 
@@ -80,5 +90,11 @@ void AToolboxBoss::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AToolboxBoss::OnMyBossBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 플레이어와 충돌했을 때 플레이어 파괴
+	//OtherActor->Destroy();
 }
 
